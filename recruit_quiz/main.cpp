95a5744d3a85b42ecb9ff99c7c756d823d6598a9
KagitaTadatoshi <kagi0408contact@gmail.com>
2024-10-09 15:13:10 +0900

    球の体積の問題を追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -46,7 +46,15 @@ int main()
 	//円錐の体積
 	x = uniform_int_distribution<>(1, 10)(rand);
 	y = uniform_int_distribution<>(1, 5)(rand) * 3;
-	questions.push_back({ "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3とする。Xの値を求めよ。", x * x * y / 3 });
+	questions.push_back({
+		"底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n"
+		+ "この円錐の体積をXπcm^3とする。Xの値を求めよ。", x * x * y / 3 });
+
+	//円の体積
+	x = uniform_int_distribution<>(1, 5)(rand) * 3;
+	questions.push_back({
+		"半径" + to_string(x) + "cmの球がある。\n"
+		+ "この球の体積をXπcm^3とする。Xの値を求めよ。", x * x * x * 4 / 3 });
 
 	cout << "[リクルート試験対策クイズ]\n";
 
